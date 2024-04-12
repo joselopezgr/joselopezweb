e7ad2f437ab1cd305d9c7688462cb4316b5576f2
Jose Lopez <jose.guerrero@tes.com>
2024-04-12 13:10:07 +0100

    working with reCaptcha3

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,10 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import nodemailer from 'nodemailer';
 import Mail from "nodemailer/lib/mailer";
+import axios from "axios";
 
-export async function POST(request: NextRequest) {
+export async function POST(request: Request, response: Response) {
 
-    const { email, name, message } = await request.json();
+    const { email, name, message, gRecaptchaToken } = await request.json();
+    const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+
+    console.log(
+        "gRecaptchaToken,firstName,lastName,email,hearFromSponsors:",
+        gRecaptchaToken?.slice(0, 10) + "...",
+        name,
+        email,
+        message
+      );
 
     const transport = nodemailer.createTransport({
         service: 'gmail',
@@ -21,21 +31,46 @@ export async function POST(request: NextRequest) {
         text: message,
     };
 
-    const sendMailPromise = () => 
-    new Promise<string>((resolve, reject) => {
-        transport.sendMail(mailOptions, function (err) {
-            if(!err) {
-                resolve('Email sent');
-            } else {
-                reject(err.message)
-            }
-        })
-    });
+    let res: any;
+    const formData = `secret=${secretKey}&response=${gRecaptchaToken}`;
 
     try {
-        await sendMailPromise();
-        return NextResponse.json({ message: 'Email sent'});
-    } catch (error) {
-        return NextResponse.json({ error }, { status: 500})
+      res = await axios.post(
+        "https://www.google.com/recaptcha/api/siteverify",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+    } catch (e) {
+      console.log("recaptcha error:", e);
     }
+
+    const sendMailPromise = () => 
+        new Promise<string>((resolve, reject) => {
+            transport.sendMail(mailOptions, function (err) {
+                if(!err) {
+                    resolve('Email sent');
+                } else {
+                    reject(err.message)
+                }
+            })
+        });
+
+    if (res && res.data?.success && res.data?.score > 0.5) {
+        // Save data to the database from here
+        console.log("res.data?.score:", res.data?.score);
+    
+        try {
+        await sendMailPromise();
+        return NextResponse.json({ success: true, name, score: res.data?.score, message: 'Email sent' })
+        } catch (error) {
+            return NextResponse.json({success: false, name, score: res.data?.score, error }, { status: 500})
+        }
+      } else {
+        console.log("fail: res.data?.score:", res.data?.score);
+        return NextResponse.json({ success: false, name, score: res.data?.score });
+      }
 }
--- src/components/contactSection.tsx
+++ src/components/contactSection.tsx
@@ -1,10 +1,9 @@
 "use client";
-
-import { FC } from "react";
-import { sendEmail } from "@/utils/sendEmail";
-import { useForm } from "react-hook-form";
-import { Fade } from "react-awesome-reveal";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import { FC, useState } from "react";
 import { Button } from "@nextui-org/react";
+import axios from "axios";
+import GoogleCaptchaWrapper from "./googleCaptchaWrapper";
 
 export type FormData = {
   name: string;
@@ -12,18 +11,72 @@ export type FormData = {
   message: string;
 };
 
-const ContactSection: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+const WrappedContactSection: FC = () => {
+  return(
+  <GoogleCaptchaWrapper>
+    <ContactSection />
+  </GoogleCaptchaWrapper>
+)}
 
-  function onSubmit(data: FormData) {
-    sendEmail(data);
-  }
+const ContactSection: FC = () => {
+  // const { register, handleSubmit } = useForm<FormData>();
+
+  // function onSubmit(data: FormData) {
+  //   sendEmail(data);
+  // }
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [notification, setNotification] = useState('');
+
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  const handleSubmitForm = function (e: any) {
+    e.preventDefault();
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not available yet");
+      setNotification(
+        "Execute recaptcha not available yet likely meaning key not set"
+      );
+      return;
+    }
+    executeRecaptcha("enquiryFormSubmit").then((gReCaptchaToken) => {
+      submitEnquiryForm(gReCaptchaToken);
+    });
+  };
+
+  const submitEnquiryForm = (gReCaptchaToken : string) => {
+    async function goAsync() {
+      const response = await axios({
+        method: "post",
+        url: "/api/email",
+        data: {
+          firstName: name,
+          email: email,
+          message: message,
+          gRecaptchaToken: gReCaptchaToken,
+        },
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response?.data?.success === true) {
+        setNotification(`Success with score: ${response?.data?.score}`);
+      } else {
+        setNotification(`Failure with score: ${response?.data?.score}`);
+      }
+    }
+    goAsync().then(() => {}); // suppress typescript error
+  };
 
   return (
     <section id="contact" className="mx-auto max-w-6xl px-6">
       <div className="my-12 pb-16 md:my-16 md:pb-20 lg:my-20 lg:pb-24 xl:my-24 xl:pb-28">
         <div className="contact-container flex flex-col space-y-10 mt-12 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
-          <form onSubmit={handleSubmit(onSubmit)} className="max-w-md w-full">
+          <form onSubmit={handleSubmitForm} className="max-w-md w-full">
             <div className="mb-5">
               <label
                 htmlFor="name"
@@ -33,9 +86,11 @@ const ContactSection: FC = () => {
               </label>
               <input
                 type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e?.target?.value)}
                 placeholder="John Doe"
                 className="w-full rounded-md border border-gray-300 opacity-80 py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
-                {...register("name", { required: true })}
               />
             </div>
             <div className="mb-5">
@@ -47,9 +102,11 @@ const ContactSection: FC = () => {
               </label>
               <input
                 type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e?.target?.value)}
                 placeholder="example@domain.com"
                 className="w-full rounded-md border border-gray-300 py-3 opacity-80 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
-                {...register("email", { required: true })}
               />
             </div>
             <div className="mb-5">
@@ -61,14 +118,15 @@ const ContactSection: FC = () => {
               </label>
               <textarea
                 rows={4}
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e?.target?.value)}
                 placeholder="Type your message"
                 className="w-full resize-none rounded-md border border-gray-300 opacity-80 py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
-                {...register("message", { required: true })}
               ></textarea>
             </div>
-            <div>
-              <Button color="warning">Submit</Button>
-            </div>
+              <Button color="warning" type="submit">Submit</Button>
+              {notification && <p className="mt-3 text-info">{notification}</p>}
           </form>
         </div>
       </div>
@@ -76,4 +134,4 @@ const ContactSection: FC = () => {
   );
 };
 
-export default ContactSection;
+export default WrappedContactSection;
