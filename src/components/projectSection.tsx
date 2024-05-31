a00315584c50d7e09595b3beb2fd413f17a50810
Jose Lopez <jose.guerrero@tes.com>
2024-05-31 16:47:01 +0100

    chore: Update project section with video demo and technology details

--- src/components/projectSection.tsx
+++ src/components/projectSection.tsx
@@ -6,64 +6,49 @@ import { Fade } from "react-awesome-reveal";
 import { Link } from "react-scroll";
 
 const ProjectSection = () => {
-  const images = [
-    {
-      src: `https://picsum.photos/400/300?random=${Math.random()}`,
-      title: "Project in progress",
-    },
-    {
-      src: `https://picsum.photos/400/300?random=${Math.random()}`,
-      title: "Project in progress",
-    },
-  ];
-
   return (
     <section id="projects" className="mx-auto max-w-6xl px-6">
       <div className="my-8 pb-16 md:my-9 md:pb-20 lg:my-14 lg:pb-24 xl:my-16 xl:pb-28">
         <div className="project-container flex justify-center items-center h-full pt-8 pb-8">
-          <div className="card1 m-4">
-            <Card className="py-4 bg-background/60 dark:bg-default-100/25 hover:bg-default-100/55" isBlurred isPressable isHoverable onPress={() => window.open("https://github.com/joselopezgr/service-submatch-user", "_blank")}>
-              <Fade direction="left" triggerOnce>
-                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                  <p style={{color: "#363636", fontStyle: "italic"}}>Work in progress</p>
-                  <small className="" style={{ color: "#8b473a"}}>
-                    Spring Boot, DDD, MongoDB, Microservices, and more
-                  </small>
-                  <h2 className="font-bold text-large">Submatch</h2>
-                </CardHeader>
-                <CardBody className="overflow-visible py-2">
-                  {" "}
-                  <Image
-                    src={images[0].src}
-                    alt={images[0].title}
-                    className="object-cover rounded-xl"
-                    width={370}
-                  />
-                </CardBody>
-              </Fade>
-            </Card>
-          </div>
-          <div className="card2 m-4">
-            <Card className="py-4 bg-background/60 dark:bg-default-100/25 hover:bg-default-100/55" isBlurred isPressable isHoverable onPress={() => window.open("https://github.com/joselopezgr/joselopezweb")}>
-              <Fade direction="right" triggerOnce>
-                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                  <p style={{color: "#363636", fontStyle: "italic"}}>Work in progress</p>
-                  <small className="" style={{ color: "#8b473a"}}>
-                    Next.js, React, Tailwind, TypeScript, and more. 
-                  </small>
-                  <h2 className="font-bold text-large">Portfolio Web</h2>
-                </CardHeader>
-                <CardBody className="overflow-visible py-2">
-                  {" "}
-                  <Image
-                    src={images[1].src}
-                    alt={images[1].title}
-                    className="object-cover rounded-xl"
-                    width={370}
-                  />
-                </CardBody>
-              </Fade>
-            </Card>
+          <div className="project-section">
+            <h1>Submatch</h1>
+            <p className="text-left ml-6">
+              This is a project that I worked on during my time at{" "}
+              <a href="https://www.tes.com/en-gb">Tes</a>. It is a substitute
+              teacher booking system that allows schools to request them based
+              on their availability and qualifications. The system also allows
+              the teachers to accept or decline the request.
+              <br />
+              <br />
+              The project was built using the following technologies:
+              <br />
+              <br />
+              <ul className="tech-list">
+                <li>Next.js & TS</li>
+                <li>Java & Spring Boot</li>
+                <li>Domain Driven Design</li>
+                <li>Hexagonal Architecture</li>
+                <li>OOP & SOLID principles</li>
+                <li>Micro-services</li>
+                <li>MongoDB</li>
+                <li>STOMP Websocket</li>
+                <li>Java Mail for SMTP</li>
+              </ul>
+            </p>
+            <p style={{color: "#d66853"}}>
+              Click or hover over to watch the video demo of the project.
+            </p>
+            <div className="video-container">
+              <video
+                className="project-video"
+                onMouseOver={(event) => event.currentTarget.play()}
+                onMouseOut={(event) => event.currentTarget.pause()}
+              >
+                <source src="/videos/submatch.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <h2 className="italic">Project code can be review under request</h2>
           </div>
         </div>
       </div>
